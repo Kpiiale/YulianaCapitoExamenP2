f5477fd5fea6e9c9978f0bf7246aa80aa370aebd
Kpiiale <yuliana.capito@udla.edu.ec>
2024-11-29 08:23:36 -0500

    Commit 2: Creacion de carpetas y modelo usuario

---
+++ YulianaCapitoExamenP2/Models/UsuarioYC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YulianaCapitoExamenP2.Models
+{
+    class UsuarioYC
+    {
+        public int Id { get; set; }
+        public int TelefonoYC { get; set; }
+        public string NombreYC { get; set; }
+    }
+}
