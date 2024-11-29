184040da337b72bc14b9db75e2152bda2c154214
Kpiiale <yuliana.capito@udla.edu.ec>
2024-11-29 08:42:45 -0500

    Commit 3: Creacion de interfaz y repositorio

---
+++ YulianaCapitoExamenP2/Interfaces/iUsuarioYCRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YulianaCapitoExamenP2.Models;
+
+namespace YulianaCapitoExamenP2.Interfaces
+{
+    interface iUsuarioYCRepository
+    {
+        List<iUsuarioYCRepository> DevuelveListadoUsuario();
+        UsuarioYC GetUsuario(int id);
+        Boolean CrearUsuario(UsuarioYC estudiante);
+        Boolean ActualizarUsuario(UsuarioYC estudiante);
+        Boolean EliminarUsuario(int id);
+    }
+}
---
+++ YulianaCapitoExamenP2/Repositories/UsuarioYCRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using YulianaCapitoExamenP2.Interfaces;
+using YulianaCapitoExamenP2.Models;
+
+namespace YulianaCapitoExamenP2.Repositories
+{
+ 
+
+    class UsuarioYCRepository : iUsuarioYCRepository
+    {
+        public bool ActualizarUsuario(UsuarioYC estudiante)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CrearUsuario(UsuarioYC estudiante)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<iUsuarioYCRepository> DevuelveListadoUsuario()
+        {
+          
+        }
+
+        public bool EliminarUsuario(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UsuarioYC GetUsuario(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
