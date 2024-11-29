a57da3826dc331ed0ea2df5fd242d89c7ee01c4b
laboratorio <laboratorio@UPOAULA10601>
2024-11-29 08:05:50 -0500

    Agregar archivos de proyecto.

---
+++ YulianaCapitoExamenP2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace YulianaCapitoExamenP2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
